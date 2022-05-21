dff80f88ab62b40479b295bf3ede18256cd3a7d8
Sasa Bakic <bakisale@gmail.com>
2022-05-22 01:08:15 +0200

    navbar refactor

--- bizkod/src/components/NavBar.tsx
+++ bizkod/src/components/NavBar.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { CaretUpFilled, CaretDownFilled } from '@ant-design/icons';
-import { Link } from 'react-router-dom';
+import { UserOutlined } from '@ant-design/icons';
+import { Avatar, Button } from 'antd';
 
 interface Props {}
 
@@ -11,9 +12,46 @@ const NavBar: React.FC<Props> = ({}) => {
     console.log('logging out');
   };
 
+  const dummyUserData = [
+    {
+      username: 'markomarkovic@infostud.com',
+      name: 'Marko Markovic',
+      image: '',
+      contact: '9304820384',
+      sector: 'Prodaja',
+      selectedEvents: [
+        {
+          id: '33333',
+          name: 'Stopama grada kroz period secesije',
+          startDate: '22.03.2022.',
+          endDate: 'ssss',
+          location: 'ssss',
+          eventTypeId: 'icvkjhdskfl',
+          eventType: 'Tura',
+          status: 'pending',
+        },
+        {
+          id: '4444',
+          name: 'Vinska tura',
+          startDate: '22.03.2022.',
+          endDate: 'ssss',
+          location: 'ssss',
+          eventTypeId: 'icvkjhdskfl',
+          eventType: 'Tura',
+          status: 'approved',
+        },
+      ],
+      users: [],
+    },
+  ];
+
   return (
     <nav className="navbar">
-      <img className="logoPicture" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_5S3Qwkc1DetbZGgcP1FTMjGFm4Poi_W7RzN3z_xP71BohZ0gHQShg2HveYtnQcADI_A&usqp=CAU" alt="logo" />
+      <img
+        className="logoPicture"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_5S3Qwkc1DetbZGgcP1FTMjGFm4Poi_W7RzN3z_xP71BohZ0gHQShg2HveYtnQcADI_A&usqp=CAU"
+        alt="logo"
+      />
       <ul className="menu-links">
         <li>
           <span>
@@ -33,14 +71,44 @@ const NavBar: React.FC<Props> = ({}) => {
           <CaretDownFilled />
         </li>
         {showDropdown && (
-          <ul className="dropdown">
-            <li style={{width: '200px'}}>
-              <Link to='/profil/2'>
-                Vidi profil
-              </Link>
-            </li>
-            <li  style={{width: '200px'}} onClick={logOut}>Odjavi se</li>
-          </ul>
+          // <ul className="dropdown">
+          //   <li style={{ width: '200px' }}>
+          //     <a href="/profil/2">Vidi prijave</a>
+          //   </li>
+          //   <li style={{ width: '200px' }} onClick={logOut}>
+          //     Odjavi se
+          //   </li>
+          // </ul>
+          <div className="profileInfo">
+            <div className="profile">
+              <div className="profilePicture">
+                {dummyUserData[0].image ? (
+                  <img
+                    className="event-img"
+                    alt="Profilna slika"
+                    src={dummyUserData[0].image}
+                  />
+                ) : (
+                  <Avatar size={64} icon={<UserOutlined />} />
+                )}
+              </div>
+              <div className="userInfo">
+                <h1>{dummyUserData[0].name}</h1>
+                <span className="profileLabel">{dummyUserData[0].sector}</span>
+                <div>
+                  <span className="profileLabel">
+                    Kontakt: {dummyUserData[0].contact}
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div className="links-container">
+              <Button type="ghost">
+                <a href="/profil/2">Vidi prijave</a>
+              </Button>
+              <Button onClick={logOut}>Odjavi se</Button>
+            </div>
+          </div>
         )}
       </ul>
     </nav>
--- bizkod/src/components/UserProfile.tsx
+++ bizkod/src/components/UserProfile.tsx
@@ -1,97 +1,92 @@
 import React, { useEffect, useState } from 'react';
 import EventCard from '../components/EventCard';
-import { useLocation, useParams } from "react-router-dom";
-import { UserOutlined } from '@ant-design/icons';
-import { Avatar } from 'antd';
+import { useLocation, useParams } from 'react-router-dom';
+
 import EventListComponent from './EventsListComponent';
 import { Events } from '../interfaces/interfaces';
 
-const dummyUserData = [{
+const dummyUserData = [
+  {
     username: 'markomarkovic@infostud.com',
     name: 'Marko Markovic',
     image: '',
     contact: '9304820384',
     sector: 'Prodaja',
     selectedEvents: [
-        {
-            id: '33333',
-            name: 'Stopama grada kroz period secesije',
-            startDate: '22.03.2022.',
-            endDate: 'ssss',
-            location: 'ssss',
-            eventTypeId: 'icvkjhdskfl',
-            eventType: 'Tura',
-            status: 'pending'
-        },
-        {
-            id: '4444',
-            name: 'Vinska tura',
-            startDate: '22.03.2022.',
-            endDate: 'ssss',
-            location: 'ssss',
-            eventTypeId: 'icvkjhdskfl',
-            eventType: 'Tura',
-            status: 'approved'
-        }
+      {
+        id: '33333',
+        name: 'Stopama grada kroz period secesije',
+        startDate: '22.03.2022.',
+        endDate: 'ssss',
+        location: 'ssss',
+        eventTypeId: 'icvkjhdskfl',
+        eventType: 'Tura',
+        status: 'pending',
+      },
+      {
+        id: '4444',
+        name: 'Vinska tura',
+        startDate: '22.03.2022.',
+        endDate: 'ssss',
+        location: 'ssss',
+        eventTypeId: 'icvkjhdskfl',
+        eventType: 'Tura',
+        status: 'approved',
+      },
     ],
-    users: [
-    ]
-}];
+    users: [],
+  },
+];
 
+interface props {}
 
-interface props { };
+const UserProfile: React.FC<props> = ({}) => {
+  let { userId } = useParams();
 
-const UserProfile: React.FC<props> = ({ }) => {
+  const [pendingEvents, setPendingEvents] = useState<Events[]>([]);
+  const [selectedEvents, setSelectedEvents] = useState<Events[]>([]);
 
-    let { userId } = useParams();
+  useEffect(() => {
+    let pending = [
+      ...dummyUserData[0].selectedEvents.filter(
+        (event) => event.status === 'pending'
+      ),
+    ];
+    setPendingEvents(pending);
+    let selected = [
+      ...dummyUserData[0].selectedEvents.filter(
+        (event) => event.status === 'approved'
+      ),
+    ];
+    setSelectedEvents(selected);
+  }, []);
 
-    const [pendingEvents, setPendingEvents] = useState<Events[]>([]);
-    const [selectedEvents, setSelectedEvents] = useState<Events[]>([]);
-
-    useEffect(() => {
-        let pending = [...dummyUserData[0].selectedEvents.filter((event) => event.status === 'pending')];
-        setPendingEvents(pending);
-        let selected = [...dummyUserData[0].selectedEvents.filter((event) => event.status === 'approved')];
-        setSelectedEvents(selected);
-    }, []);
-
-    return (
-        <div className="userProfileContainer">
-            <div className="profileInfo">
-                <div className="profile">
-                    <div className="profilePicture">
-                        {dummyUserData[0].image ?
-                            <img className='event-img' alt='Profilna slika' src={dummyUserData[0].image} />
-                            : <Avatar size={64} icon={<UserOutlined />} />
-                        }
-                    </div>
-                    <div className="userInfo">
-                        <h1>{dummyUserData[0].name}</h1>
-                        <span className='profileLabel'>{dummyUserData[0].sector}</span>
-                        <div>
-                            <span className='profileLabel'>Kontakt: {dummyUserData[0].contact}</span>
-                        </div>
-                    </div>
-
-                </div>
+  return (
+    <div className="userProfileContainer">
+      <div className="eventsContainer">
+        {pendingEvents ? (
+          <>
+            <div>
+              <EventListComponent
+                eventsList={pendingEvents}
+                listTitle={'Na čekanju'}
+                state={'awaiting'}
+              ></EventListComponent>
             </div>
-            <div className="eventsContainer">
-                {pendingEvents ?
-                    <>
-                        <div>
-                           <EventListComponent eventsList={pendingEvents} listTitle={"Na čekanju"} state={"awaiting"}></EventListComponent>
-                        </div>
-                    </>
-                    :
-                    <></>
-                }
-                <div>
-                    <EventListComponent eventsList={selectedEvents} listTitle={"Odabrani događaji"} state={"selected"}></EventListComponent>
-                </div>
-            </div>
+          </>
+        ) : (
+          <></>
+        )}
+        <div>
+          <EventListComponent
+            eventsList={selectedEvents}
+            listTitle={'Odabrani događaji'}
+            state={'selected'}
+          ></EventListComponent>
         </div>
-    )
-}
-
+      </div>
+    </div>
+  );
+};
 
 export default UserProfile;
