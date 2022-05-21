fb505883f4d5891841a727d3cc7729b113a56d00
tdrinic <teodoradrinic37@gmail.com>
2022-05-21 21:20:38 +0200

    Added interfaces

---
+++ bizkod/src/interfaces/interfaces.tsx
@@ -0,0 +1,11 @@
+// events
+export interface Events {
+    id: string,
+    name: string,
+    startDate: string,
+    endDate: string,
+    location: string,
+    eventTypeId: string,
+    eventType: string
+}
+
