68310da40ee3bf40c971bbc4345b3c1cf4d2eb47
candy <alek.kenderesi@gmail.com>
2022-05-21 22:58:09 +0200

    login

--- bizkod/src/App.tsx
+++ bizkod/src/App.tsx
@@ -1,13 +1,20 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 // import { EventViewModal } from './modals/EventViewModal';
-import EventCard from './components/EventCard';
-import NavBar from './components/NavBar';
-import Routes from './components/Routes';
-import FormModal from './components/FormModal';
+import EventCard from "./components/EventCard";
+import NavBar from "./components/NavBar";
+import Routes from "./components/Routes";
+import FormModal from "./components/FormModal";
+import useToken from "./components/Login/UseToken";
+import Login from "./components/Login/Login";
 
 function App() {
   const [show, setShow] = useState(true);
+  const { token, setToken } = useToken();
+
+  if (!token) {
+    return <Login setToken={setToken} />;
+  }
 
   return (
     <div className="App">
